5e56aac34301c621c76f593d991f0d89ec2925f3
Sergiy Vlasov <s_vlasov@ukr.net>
2025-06-17 18:54:50 +0300

    feat: update 3rd party libs

---
+++ aixm-client/eslint.config.mjs
@@ -0,0 +1,54 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([
+    globalIgnores(["projects/**/*", "**/dist", "**/docker", "**/node_modules"]),
+    {
+        files: ["**/*.ts"],
+
+        extends: compat.extends(
+            "eslint:recommended",
+            "plugin:@typescript-eslint/recommended",
+            "plugin:@angular-eslint/recommended",
+            "plugin:@angular-eslint/template/process-inline-templates",
+        ),
+
+        rules: {
+            "@angular-eslint/directive-selector": ["error", {
+                type: "attribute",
+                prefix: "app",
+                style: "camelCase",
+            }],
+
+            "@angular-eslint/component-selector": ["error", {
+                type: "element",
+                prefix: "app",
+                style: "kebab-case",
+            }],
+
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+        },
+    },
+    {
+        files: ["**/*.html"],
+
+        extends: compat.extends(
+            "plugin:@angular-eslint/template/recommended",
+            "plugin:@angular-eslint/template/accessibility",
+        ),
+
+        rules: {},
+    },
+]);
